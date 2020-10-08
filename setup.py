2355c0b5eb845892ce5c7250a98e1b292f6dbaaf
marc131183 <48160785+marc131183@users.noreply.github.com>
2020-10-08 11:51:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Marc_Guppy',
+    url='https://github.com/marc131183/marc_guppy',
+    author='Marc Gröling',
+    packages=['marc_guppy'],
+    install_requires=['numpy, stable_baselines, scipy, gym_guppy'],
+    version='0.1',
+    description='Custom Guppy with rl model',
+)
